fdb444675946df92cc44936e969b7342dde0a4ed
sambit221 <56729866+sambit221@users.noreply.github.com>
2021-04-03 16:15:19 +0530

    updated

---
+++ CF/WayTooLongWords.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[]){
+    int n, len=0;
+    string str;
+    cin>>n;
+    while (n){
+        cin>>str;
+        if (str.length()>10){
+            len = str.length()-2;
+            cout<<str[0]<<len<<str[str.length()-1]<<endl;
+        }
+        else{
+            cout<<str<<endl;
+        }       
+        n--;
+    }   
+    return 0;
+}
